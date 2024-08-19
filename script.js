942933ffbda1f41e0020997c381f61b149a2f86e
Christian Johnson <christianjohnson@Christians-MacBook-Pro.local>
2024-08-19 00:11:27 -0400

    project_push

---
+++ script.js
@@ -0,0 +1,26 @@
+let images = [
+  "assets/dice-01.svg",
+  "assets/dice-02.svg",
+  "assets/dice-03.svg",
+  "assets/dice-04.svg",
+  "assets/dice-05.svg",
+  "assets/dice-06.svg",
+];
+
+let dice = document.querySelectorAll("img");
+
+function roll() {
+  dice.forEach((die) => {
+    die.classList.add("shake");
+  });
+
+  setTimeout(() => {
+    dice.forEach((die) => {
+      die.classList.remove("shake");
+    });
+    let dieOneVal = Math.floor(Math.random() * 6);
+    let dieTwoVal = Math.floor(Math.random() * 6);
+    document.querySelector("#die-1").setAttribute("src", images[dieOneVal]);
+    document.querySelector("#die-2").setAttribute("src", images[dieTwoVal]);
+  }, 1000);
+}
